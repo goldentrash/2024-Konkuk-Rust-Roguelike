a4c8b24ee41ed7b909462510cd341eb8c360594c
WhitePiano <startoflateness@gmail.com>
2024-11-04 19:12:51 +0900

    feat : assignments new 6-3

--- 3 - Assignment/240704 - Rust Basic + Roguelike Game, Week 6/task-scheduler/src/lib.rs
+++ 3 - Assignment/240704 - Rust Basic + Roguelike Game, Week 6/task-scheduler/src/lib.rs
@@ -4,7 +4,6 @@ use std::rc::Rc;
 type SharedState = Rc<RefCell<i32>>;
 type Task = Box<dyn FnMut() -> String>;
 
-#[allow(dead_code)]
 pub struct TaskScheduler {
     tasks: Vec<Task>,
     shared_state: SharedState,
@@ -13,30 +12,41 @@ pub struct TaskScheduler {
 impl TaskScheduler {
     // Create a new TaskScheduler with shared state initialized to 0.
     pub fn new() -> Self {
-        todo!()
+        Self {
+            tasks: vec![],
+            shared_state: SharedState::new(RefCell::new(0)),
+        }
     }
 
     // Register a stateful task.
-    pub fn register_task<F>(&mut self, _task: F)
+    pub fn register_task<F>(&mut self, task: F)
     where
         F: FnMut() -> String + 'static,
     {
-        todo!()
+        self.tasks.push(Box::new(task));
     }
 
     // Execute tasks sequentially and stop if any task returns "fail".
     pub fn execute_tasks(&mut self) -> Vec<String> {
-        todo!()
+        let mut result = vec![];
+        for task in &mut self.tasks {
+            result.push(task.as_mut()());
+            if result.last().unwrap().eq("fail") {
+                break;
+            }
+        }
+        result
     }
 
     // Restart the scheduler by resetting shared state and re-running all tasks.
     pub fn restart(&mut self) -> Vec<String> {
-        todo!()
+        self.shared_state.replace(0);
+        self.execute_tasks()
     }
 
     // Get the shared state value.
     pub fn get_state(&self) -> i32 {
-        todo!()
+        *self.shared_state.borrow()
     }
 }
 
